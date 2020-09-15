7e181c03a7dbf476ecbc4c5faffb3704f79d767f
Joe Bruggeman <joe@bruggeman.ca>
2020-09-15 16:12:56 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let scene = fs::read_to_string("scene.json")
+        .expect("Something went wrong reading the file");
+
+    println!("Scene: {}", scene);
+
+
+}
