f27a82d30c4b5cde8ee28706d410101d3478734f
Joe Bruggeman <joe@bruggeman.ca>
2020-10-22 23:11:26 -0400

    Some cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::fs;
-use raytracer::raytracer::json_to_scene;
+mod scene;
 
 fn main() {
-    let scene_str = fs::read_to_string("scene.json")
+    let scene_str = std::fs::read_to_string("scene.json")
         .expect("Something went wrong reading the file");
 
     println!("Scene: {}", scene_str);
 
-    let scene = json_to_scene(&scene_str).expect("Error parsing JSON");
+    let scene = scene::Scene::from_json(&scene_str).expect("Error parsing JSON");
 
     println!("Scene width: {}", scene.viewport.width);
 }
