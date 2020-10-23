ee33c18410c653b145db7f4fb0e8a8ef24327ec2
Joe Bruggeman <joe@bruggeman.ca>
2020-10-22 22:36:49 -0400

    It compiles!

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,62 @@
-mod ray_trader {
+pub mod raytracer {
+    use serde_json::{Value};
+    use serde::Deserialize;
+
+    #[derive(Deserialize, Debug)]
     struct Point {
         x: f64,
         y: f64,
         z: f64,
     }
 
+    #[derive(Deserialize, Debug)]
     struct Color {
-        r: u8,
-        g: u8,
-        b: u8,
-        a: u8,
+        color: String
     }
 
+    #[derive(Deserialize, Debug)]
     struct Viewport {
         width: u32,
         height: u32,
     }
 
+    #[derive(Deserialize, Debug)]
     struct Camera {
         position: Point,
         target: Point,
         fov: f64
     }
 
-    enum ObjectType {
-        Sphere,
-        Light
-    }
-
+    #[derive(Deserialize, Debug)]
     struct Sphere {
         color: Color,
         position: Point,
         radius: f64
     }
 
+    #[derive(Deserialize, Debug)]
     struct Light {
         position: Point
     }
 
+    #[derive(Deserialize, Debug)]
     struct Objects {
         spheres: Vec<Sphere>,
         lights: Vec<Light>,
+    }
+
+    #[derive(Deserialize, Debug)]
+    pub struct Scene {
+        viewport: Viewport,
+        camera: Camera,
+        objects: Objects
+    }
+
+    pub fn json_to_scene(scene_str: &String) -> serde_json::Result<Scene> {
+        let v: Value = serde_json::from_str(scene_str)?;
+
+        let scene: Scene = (serde_json::from_value(v))?;
+
+        Ok(scene)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::fs;
-
+use raytracer::raytracer::json_to_scene;
 
 fn main() {
-    let scene = fs::read_to_string("scene.json")
+    let scene_str = fs::read_to_string("scene.json")
         .expect("Something went wrong reading the file");
 
-    println!("Scene: {}", scene);
+    println!("Scene: {}", scene_str);
 
-
+    let scene = json_to_scene(&scene_str);
 }
