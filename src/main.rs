b3a90b37bad5675dd51158a44db95aa26665f7bc
Joe Bruggeman <joe@bruggeman.ca>
2020-10-26 13:59:28 -0400

    Add basic bitmap output

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+#[macro_use]
+extern crate bmp;
+use bmp::{Image, Pixel};
+
 mod scene;
 
 fn main() {
     let scene_str = std::fs::read_to_string("scene.json")
         .expect("Something went wrong reading the file");
 
-    println!("Scene: {}", scene_str);
-
     let scene = scene::Scene::from_json(&scene_str).expect("Error parsing JSON");
+    let mut img = Image::new(scene.viewport.width, scene.viewport.height);
 
-    println!("Scene width: {}", scene.viewport.width);
+    for (x, y) in img.coordinates() {
+        let color: scene::Color = scene::compute_pixel_from_scene(&scene);
+        img.set_pixel(x, y, px!(color.r, color.g, color.b));
+    }
+
+    let _ = img.save("output.bmp");
 }
---
+++ src/scene.rs
@@ -0,0 +1,71 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Viewport {
+    pub width: u32,
+    pub height: u32,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Camera {
+    pub position: Point,
+    pub target: Point,
+    pub fov: f64
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Sphere {
+    pub color: Color,
+    pub position: Point,
+    pub radius: f64
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Light {
+    pub position: Point
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Objects {
+    pub spheres: Vec<Sphere>,
+    pub lights: Vec<Light>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Scene {
+    pub viewport: Viewport,
+    pub camera: Camera,
+    pub objects: Objects
+}
+
+impl Scene {
+    pub fn from_json(scene_str: &String) -> serde_json::Result<Scene> {
+        let v: serde_json::Value = serde_json::from_str(scene_str)?;
+
+        let scene: Scene = (serde_json::from_value(v))?;
+
+        Ok(scene)
+    }
+}
+
+pub fn compute_pixel_from_scene(scene: &Scene) -> Color {
+    Color {
+        r: 128,
+        g: 0,
+        b: 128
+    }
+}
