3034b186a971c7d640de93e527d600432b2445fb
Joe Bruggeman <joe@bruggeman.ca>
2020-10-29 21:46:39 -0400

    add closest sphere calcuations

--- src/generate.rs
+++ src/generate.rs
@@ -42,6 +42,7 @@ fn compute_ray(scene: &Scene, x: u32, y: u32) -> Line3D {
     }
 }
 
+#[derive(Copy, Clone, Debug)]
 pub struct Intersection {
     sphere: Sphere,
     point: Point3D
@@ -53,6 +54,11 @@ pub fn closest_point_of_intersection(sphere: &Sphere, ray: &Line3D) -> Option<Po
     let cz = sphere.position.z;
 
     let p1 = ray.origin;
+    let p2 = Point3D {
+        x: ray.origin.x + ray.vec.x,
+        y: ray.origin.y + ray.vec.y,
+        z: ray.origin.z + ray.vec.z
+    };
 
     let px = p1.x;
     let py = p1.y;
@@ -71,31 +77,69 @@ pub fn closest_point_of_intersection(sphere: &Sphere, ray: &Line3D) -> Option<Po
     let d = b * b - 4.0 * a * c;
  
     if d < 0.0 {
-        Option::None
-    } else if d == 0.0 {
-        Option::Some(Point3D::zero())
+        return Option::None;
+    }
+
+    let t1 = ( -b - d.sqrt() ) / ( 2.0 * a );
+    let solution1 = Point3D {
+        x: p1.x * ( 1.0 - t1 ) + t1 * p2.x,
+        y: p1.y * ( 1.0 - t1 ) + t1 * p2.y,
+        z: p1.z * ( 1.0 - t1 ) + t1 * p2.z
+    };
+        
+    if d == 0.0 {
+        return Option::Some(solution1);
+    } 
+
+    let t2 = ( -b + d.sqrt() ) / ( 2.0 * a );
+    let solution2 = Point3D {
+        x: p1.x * ( 1.0 - t2 ) + t2 * p2.x,
+        y: p1.y * ( 1.0 - t2 ) + t2 * p2.y,
+        z: p1.z * ( 1.0 - t2 ) + t2 * p2.z
+    };
+
+    let sol1_vec3d = Vector3D::from_points(&ray.origin, &solution1);
+    let sol2_vec3d = Vector3D::from_points(&ray.origin, &solution2);
+
+    if sol1_vec3d.length() < sol2_vec3d.length() {
+        Option::Some(solution1)
     } else {
-        Option::Some(Point3D::zero())
+        Option::Some(solution2)
     }
 }
 
 pub fn get_closest_sphere(scene: &Scene, ray: &Line3D) -> Option<Intersection> {
-    let mut ret = Option::None;
+    let mut closest_sphere = Option::None;
 
     // Hack
     for sphere in &scene.objects.spheres {
         match closest_point_of_intersection(&sphere, &ray) {
             Some(point) => {
-                ret = Option::Some(Intersection {
-                    sphere: *sphere,
-                    point: point
-                });
+                match closest_sphere {
+                    None => {
+                        closest_sphere = Option::Some(Intersection {
+                            sphere: *sphere,
+                            point: point
+                        });
+                    },
+                    Some(current_closest) => {
+                        let current_point = Vector3D::from_points(&ray.origin, &current_closest.point);
+                        let new_point = Vector3D::from_points(&ray.origin, &point);
+
+                        if new_point.length() < current_point.length() {
+                            closest_sphere = Option::Some(Intersection {
+                                sphere: *sphere,
+                                point: point
+                            });
+                        }
+                    }
+                }
             },
             None => ()
         }
     }
 
-    ret
+    closest_sphere
 }  
 
 pub fn compute_color(scene: &Scene, intersection: &Intersection) -> Color {
